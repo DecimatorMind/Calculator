7cc908c4514bdff57940916e085c8e2873ba821d
Pranjal Bhardwaj <E19CSE432@bennett.edu.in>
2020-04-23 06:13:18 +0530

    Add button functions and update Display Label.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,12 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var isFinishedTypingNumber: Bool = true
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {fatalError("Cannot convert text to double")}
+        
+        if let calcMethod = sender.currentTitle {
+            
+            if calcMethod == "+/-" {
+                
+                if displayLabel.text != "0" {
+                    
+                    displayLabel.text = String(number * -1)
+                    
+                }
+                
+            } else if calcMethod == "AC" {
+                
+                displayLabel.text = "0"
+                
+            } else if calcMethod == "%" {
+                
+                displayLabel.text = String(number * 0.01)
+                
+            }
+            
+            
+        }
+        
+        
+    
+        
     }
-
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                
+                displayLabel.text = numValue
+                
+                isFinishedTypingNumber = false
+                
+            } else {
+                
+                displayLabel.text = displayLabel.text! + numValue
+                
+            }
+            
+        }
+        
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    }
 
 }
 
